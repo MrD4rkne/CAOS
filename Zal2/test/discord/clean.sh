0c97b76bf24baa6dc6d54cd0167e40e071045981
Marcin <ms459531@students.mimuw.edu.pl>
2024-07-04 13:01:58 +0200

    chore: remove not mine tests

--- Zal2/test/discord/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf mdiv.asm
-rm -rf ./build
-rm -rf ./test/a.out
-rm -rf ./test/b.out
-rm -rf ./test/test.in
--- Zal3/tests/discord/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bash ./run_exit_code_tests.sh
-bash ./run_large_tests.sh
-bash ./run_sample_tests.sh
